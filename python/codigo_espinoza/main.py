a6be7441166c114dc4bdd8b80b73441cb7c2598f
Walicarpio <eberrios@gmail.com>
2026-01-22 22:38:31 -0300

    update tkinter

--- python/codigo_espinoza/main.py
+++ python/codigo_espinoza/main.py
@@ -5,4 +5,14 @@ from openpyxl import Workbook
 wb = Workbook()
 ws = wb.active
 ws.append(['Nombre', 'Edad', 'Email', 'Teléfono', 'Dirección'])
-wb.save('datos.xlsx')
+
+root = tk.Tk()
+root.title("Formulario de Entrada de Datos")
+root.configure(bg='#4b6587')
+label_style = {'bg': '#4b6587', 'fg': 'white'}
+entry_style = {'bg': '#D3D3D3', 'fg': 'black'}
+
+label_nombre = tk.Label(root, text='Nombre', **label_style)
+label_nombre.grid(row=0, column=0, padx=10, pady=5)
+
+root.mainloop()
